ca02d8d18d1970ed9fd4e2f149153e4184312820
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-10-12 09:15:44 +0200

    project structuring

---
+++ UniSave/unisave/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request):
+    pass
+
+def signin_view(request):
+    pass
+
+def signup_view(request):
+    pass
+
+def signout_view(request):
+    pass
